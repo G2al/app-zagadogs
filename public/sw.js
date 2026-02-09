c3ff9fd6050cb2e3582255fad61fdcd4d0578771
G2al <g2aluigi@gmail.com>
2026-02-09 15:50:07 +0100

    sistemato pwa si spera

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,13 @@
-const CACHE_NAME = 'zagadogs-pwa-v2';
+const CACHE_NAME = 'zagadogs-pwa-v3';
 const STATIC_ASSETS = [
   '/offline.html',
   '/manifest.webmanifest',
-  '/icons/icon-180.png',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
-  '/icons/icon-192-maskable.png',
-  '/icons/icon-512-maskable.png',
+  '/icons/icon-180-v2.png',
+  '/icons/icon-192-v2.png',
+  '/icons/icon-512-v2.png',
+  '/icons/icon-192-maskable-v2.png',
+  '/icons/icon-512-maskable-v2.png',
+  '/pwa.css?v=2',
 ];
 
 self.addEventListener('install', (event) => {
